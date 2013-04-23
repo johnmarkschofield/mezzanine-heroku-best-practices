e597538856c5c666f065c49100f8f77e55e5eae2
John Mark Schofield <jms@schof.org>
2013-04-22 17:49:02 -0700

    Updating goheroku script.

--- mhbp_goheroku.bash
+++ mhbp_goheroku.bash
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-set -e
-
 while read line; do
     eval export $line
 done < mhbp_settings
 
 source $VIRTUALENV_HOME/$VIRTUALENV_NAME/bin/activate
 
+
+git status | grep "nothing to commit"
+if [ $? -ne 0 ]; then
+    echo "First, check in all your changes to git. Then try again."
+    exit 1
+fi
+
+echo "Pushing static assets to S3"
 ./mhbp_collectstatic.bash
+
+echo "Pushing code to Heroku"
 git push heroku master
